63801f29dce1dbd8d4d3f0561f955ecc15ecc342
rsByteShark <->
2023-10-30 01:32:57 +0100

    t4

--- utils/vercelRestApiUtils.js
+++ utils/vercelRestApiUtils.js
@@ -169,7 +169,7 @@ module.exports.getVercelProjectEnvs = (restToken, projectName) => {
 
                 if (res.headers["content-type"].includes("application/json")) {
 
-                    const projectEnv = JSON.parse(gatheredData.toString())
+                    const projectEnv = JSON.parse(gatheredData.toString()).env
 
                     resolve(projectEnv);
 
