60ca5b923b97bf50cac32f19d824d86b1af93358
rsByteShark <->
2023-11-02 14:05:17 +0100

    t12

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # General Notes
 This is pseudo-ecommerce shop web app with fully functional login, order creation system, integration with database and external api, that i develop during my learning sessions about structure of web applications and api security.
+
+# Predeploy notes
+To prevent errors during build:
+- create Vercel Postgres storage
+- connect created storage to your project
+- generate vercel api key for your vercel account
+- create env var "API_KEY" 
--- prodInitScript.js
+++ prodInitScript.js
@@ -3,6 +3,7 @@ const { spawn } = require('child_process');
 const { generateAsymmetricPemEncodedRSAKeysForJWT, populateDBwithFakestoreapiData } = require("./utils/generalBackendUtils")
 
 const projectName = "cool-shop";
+const fakeapiproductsimagespath = `${__dirname}/public/fakeapiproductsimages`;
 
 (async () => {
 
@@ -62,7 +63,7 @@ const projectName = "cool-shop";
 
             //populate db with fakestoreapidata
 
-            await populateDBwithFakestoreapiData();
+            await populateDBwithFakestoreapiData(fakeapiproductsimagespath);
 
             console.log('Starting next build...\n\n');
 
